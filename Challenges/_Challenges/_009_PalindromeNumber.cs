75db78720484a364eb149279822f9264fc601f7b
Alberto <betotl.garcia@gmail.com>
2022-08-23 15:09:43 -0400

    add Palindrome Number challenge and test

---
+++ Challenges/_Challenges/_009_PalindromeNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges._Challenges {
+    public class _009_PalindromeNumber {
+        /* 
+         * Given an integer x, return true if x is palindrome integer.
+         * An integer is a palindrome when it reads the same backward as forward.
+        */
+        public static bool IsPalindrome(int number) {
+            string numberString = number.ToString();
+            string isPalindrome = "";
+            for (int i = numberString.Length - 1; i >= 0; i--) {
+                isPalindrome += numberString[i];
+            }
+            return isPalindrome == numberString;
+        }
+    }
+}
---
+++ Tests/_Tests/_009_PalindromNumberTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Tests._Tests {
+    [TestClass]
+    public class _009_PalindromNumberTest {
+
+        [TestMethod]
+        public void Test_001() {
+            int inputNumber = 121;
+            bool expectedResult = true;
+            bool userSolution = _009_PalindromeNumber.IsPalindrome(inputNumber);
+            Assert.AreEqual(expectedResult, userSolution);
+        }
+
+        [TestMethod]
+        public void Test_002() {
+            int inputNumber = 123456789;
+            bool expectedResult = false;
+            bool userSolution = _009_PalindromeNumber.IsPalindrome(inputNumber);
+            Assert.AreEqual(expectedResult, userSolution);
+        }
+    }
+}
