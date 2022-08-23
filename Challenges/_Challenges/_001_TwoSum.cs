f9e5888c3c0718b02ce183be1d8f1d4e1efffe0a
Alberto <betotl.garcia@gmail.com>
2022-08-23 14:46:12 -0400

    add two sum challenge solution and test

---
+++ Challenges/_Challenges/_001_TwoSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges._Challenges {
+    public class _001_TwoSum {
+        /* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order. */
+
+        public static int[] Solution(int[] inputArray, int target) {
+            for (int i = 0; i < inputArray.Length; i++) {
+                int complement = target - inputArray[i];
+                for (int j = 0; j < inputArray.Length; j++) {
+                    if (i == j) continue;
+                    if (inputArray[j] == complement) return new int[] { i, j };
+                }
+            }
+            return new int[0];
+        }
+    }
+}
---
+++ Tests/_Tests/_001_TwoSumTest.cs
@@ -0,0 +1,31 @@
+﻿using Challenges._Challenges;
+
+namespace Tests._Tests {
+    [TestClass]
+    public class _001_TwoSumTest {
+
+        [TestMethod]
+        public void Test_001() {
+            int[] inputArray = new int[] { 2, 7, 11, 15 };
+            int inputTarget = 9;
+            int[] expectedArray = new int[] { 0, 1 };
+            CollectionAssert.AreEqual(expectedArray, _001_TwoSum.Solution(inputArray, inputTarget));
+        }
+
+        [TestMethod]
+        public void Test_002() {
+            int[] inputArray = new int[] { 3, 2, 4 };
+            int inputTarget = 6;
+            int[] expectedArray = new int[] { 1, 2 };
+            CollectionAssert.AreEqual(expectedArray, _001_TwoSum.Solution(inputArray, inputTarget));
+        }
+
+        [TestMethod]
+        public void Test_003() {
+            int[] inputArray = new int[] { 3, 3 };
+            int inputTarget = 6;
+            int[] expectedArray = new int[] { 0, 1 };
+            CollectionAssert.AreEqual(expectedArray, _001_TwoSum.Solution(inputArray, inputTarget));
+        }
+    }
+}
