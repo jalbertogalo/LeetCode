e3802c0c23f0ee2690b02652e406cad95dc77751
Alberto <betotl.garcia@gmail.com>
2022-08-23 23:41:41 -0400

    add Roman to Integer challenge and tests

---
+++ Challenges/_Challenges/_013_RomanToInteger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges._Challenges {
+    /* 
+     * Given a roman numeral, convert it to an integer
+     */
+    public class _013_RomanToInteger {
+        public Dictionary<char, int> RomanNumbers { get; }
+
+        public _013_RomanToInteger() {
+            RomanNumbers = new Dictionary<char, int>();
+            RomanNumbers.Add('I', 1);
+            RomanNumbers.Add('V', 5);
+            RomanNumbers.Add('X', 10);
+            RomanNumbers.Add('L', 50);
+            RomanNumbers.Add('C', 100);
+            RomanNumbers.Add('D', 500);
+            RomanNumbers.Add('M', 1000);
+        }
+
+        public int RomanToInt(string romanNumber) {
+            int romanToInt = RomanNumbers[romanNumber[romanNumber.Length - 1]];
+            for (int i = romanNumber.Length - 1; i >= 1; i--) {
+                int currentPositionValue = RomanNumbers[romanNumber[i]];
+                int nextPositionValue = RomanNumbers[romanNumber[i - 1]];
+                romanToInt = currentPositionValue <= nextPositionValue ? romanToInt + nextPositionValue : romanToInt - nextPositionValue;
+            }
+            return romanToInt;
+        }
+    }
+}
---
+++ Tests/_Tests/_013_RomanToIntegerTest.cs
@@ -0,0 +1,49 @@
+﻿namespace Tests._Tests {
+    [TestClass]
+    public class _013_RomanToIntegerTest {
+
+        [TestMethod]
+        public void Test_001() {
+            string inputRomanNumber = "III";
+            int expectedValue = 3;
+
+            _013_RomanToInteger romanToInteger = new _013_RomanToInteger();
+
+            int solution = romanToInteger.RomanToInt(inputRomanNumber);
+            Assert.AreEqual(expectedValue, solution);
+        }
+
+        [TestMethod]
+        public void Test_002() {
+            string inputRomanNumber = "IV";
+            int expectedValue = 4;
+
+            _013_RomanToInteger romanToInteger = new _013_RomanToInteger();
+
+            int solution = romanToInteger.RomanToInt(inputRomanNumber);
+            Assert.AreEqual(expectedValue, solution);
+        }
+
+        [TestMethod]
+        public void Test_003() {
+            string inputRomanNumber = "LX";
+            int expectedValue = 60;
+
+            _013_RomanToInteger romanToInteger = new _013_RomanToInteger();
+
+            int solution = romanToInteger.RomanToInt(inputRomanNumber);
+            Assert.AreEqual(expectedValue, solution);
+        }
+
+        [TestMethod]
+        public void Test_004() {
+            string inputRomanNumber = "XLIII";
+            int expectedValue = 43;
+
+            _013_RomanToInteger romanToInteger = new _013_RomanToInteger();
+
+            int solution = romanToInteger.RomanToInt(inputRomanNumber);
+            Assert.AreEqual(expectedValue, solution);
+        }
+    }
+}
