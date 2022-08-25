e3d2248267ed97d467c0484a95fdaf2513b550ef
Alberto Lopez <jalberto.logar@gmail.com>
2022-08-25 00:51:35 -0400

    add ValidParentheses challenge and test

---
+++ Challenges/_Challenges/_020_ValidParenthesis.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges._Challenges {
+    /* 
+     * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+     */
+    public class _020_ValidParenthesis {
+        public static bool IsValid(string inputArray) {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in inputArray) {
+                switch (c) {
+                    case '[':
+                    case '{':
+                    case '(': stack.Push(c); break;
+                    case ')': if (stack.Count == 0 || stack.Peek() != '(') return false; else stack.Pop(); break;
+                    case '}': if (stack.Count == 0 || stack.Peek() != '{') return false; else stack.Pop(); break;
+                    case ']': if (stack.Count == 0 || stack.Peek() != '[') return false; else stack.Pop(); break;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
